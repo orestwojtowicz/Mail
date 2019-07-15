4ece75d9ff3d2303e857624f30435020b62b3744
orestwojtowicz <damianwojtowicz94@gmail.com>
2019-07-15 09:21:09 +0200

    Initial commit

---
+++ src/test/java/com/damian/MainTest.java
@@ -0,0 +1,15 @@
+package com.damian;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class MainTest {
+
+    @Test
+    public void Test() {
+        Assert.assertTrue(true);
+    }
+
+}
